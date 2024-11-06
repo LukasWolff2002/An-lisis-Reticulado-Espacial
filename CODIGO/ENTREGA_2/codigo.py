9e358814a93668f8bf20e67c210fdea06fca4e22
LukasWolff2002 <lwolff@miuandes.cl>
2024-11-06 11:06:34 -0300

    avance

--- CODIGO/ENTREGA_2/codigo.py
+++ CODIGO/ENTREGA_2/codigo.py
@@ -1,8 +1,12 @@
+# Importación de módulos necesarios
 import numpy as np
 import openseespy.opensees as ops
 import pyvista as pv
 import matplotlib.pyplot as plt
-from variables import E, A, gamma, num_capas, unidad, gamma_rigido, masa_total, Area_panel, alpha, deltaT, barras_centrales
+from variables import (
+    E, A, gamma, num_capas, unidad, gamma_rigido, masa_total,
+    Area_panel, alpha, deltaT, barras_centrales
+)
 
 # --- Configuración Inicial del Modelo ---
 def inicializar_modelo():
@@ -28,9 +32,8 @@ def crear_elemento_truss(element_id, nodo_i, nodo_j, A, material_id, gamma):
         raise ValueError("El área de la sección transversal (A) debe ser mayor que 0.")
     if gamma < 0:
         raise ValueError("La densidad (gamma) no puede ser negativa.")
-    
     ops.element('Truss', element_id, nodo_i, nodo_j, A, material_id, '-rho', gamma)
-    return (nodo_i, nodo_j)
+    return (element_id, nodo_i, nodo_j)
 
 def conectar_nodos_cuadrado(node_tags, element_id, A, material_id, gamma, elements):
     for i in range(len(node_tags)):
@@ -82,7 +85,7 @@ def generar_capas(nodes, num_capas, element_id, A, material_id, gamma, elements)
 
     return element_id
 
-# --- Agregar Estructura Flexible ---
+# --- Agregar Estructura de Paneles ---
 def agregar_estructura_flexible(nodos_conectados, A_flexible, gamma_flexible, element_id, elements, material_id_flexible):
     for i in range(len(nodos_conectados)):
         j = (i + 1) % len(nodos_conectados)
@@ -90,36 +93,9 @@ def agregar_estructura_flexible(nodos_conectados, A_flexible, gamma_flexible, el
             crear_elemento_truss(element_id, nodos_conectados[i], nodos_conectados[j], A_flexible, material_id_flexible, gamma_flexible)
         )
         element_id += 1
-
     return element_id
 
-# --- Patrón de Inercia ---
-def inercia ():
-    paternx = 1
-    paterny = 2
-    paternz = 3
-
-    ax = [0.1 * 9.81]  # Aceleración en x como lista
-    ay = [0.1 * 9.81]  # Aceleración en y como lista
-    az = [0.1 * 9.81]  # Aceleración en z como lista
-
-    dirx = 1
-    diry = 2
-    dirz = 3
-
-    ops.timeSeries('Constant', paternx, '-values', *ax)
-    ops.timeSeries('Constant', paterny, '-values', *ay)
-    ops.timeSeries('Constant', paternz, '-values', *az)
-
-    #Esto baja los desplazamiento pero no genera un cambio
-    #ops.timeSeries('Linear', paternx, '-factor', ax[0])
-    #ops.timeSeries('Linear', paterny, '-factor', ay[0])
-    #ops.timeSeries('Linear', paternz, '-factor', az[0])
-
-    ops.pattern('UniformExcitation', paternx, dirx, '-accel', paternx)
-    ops.pattern('UniformExcitation', paterny, diry, '-accel', paterny)
-    ops.pattern('UniformExcitation', paternz, dirz, '-accel', paternz)
-
+# --- Aplicación de Cargas Inerciales ---
 def aplicar_cargas_inerciales(masa_acumulada_por_nodo):
     acceleration_x = 0.1 * 9.81  # Aceleración en X (m/s²)
     acceleration_y = 0.1 * 9.81  # Aceleración en Y (m/s²)
@@ -133,52 +109,40 @@ def aplicar_cargas_inerciales(masa_acumulada_por_nodo):
         F_inertial_z = -masa * acceleration_z  # Fuerza inercial en Z
         ops.load(nodo, F_inertial_x, F_inertial_y, F_inertial_z)
 
-# --- Asignación de Masa de Paneles ---
-def acumular_masa_paneles(conexiones_paneles, masa_total, masa_acumulada_por_nodo):
-    """
-    Agrega la masa de los paneles a los nodos específicos que están conectados a los paneles.
-    """
-    masa_por_panel = masa_total / 4
-
-    for nodos_conectados in conexiones_paneles:
-        for nodo in nodos_conectados:
-            masa_acumulada_por_nodo[nodo] = masa_acumulada_por_nodo.get(nodo, 0) + masa_por_panel
-
-    return masa_acumulada_por_nodo
-
-# Para asignar la masa acumulada a los nodos
-def asignar_masas_a_nodos(masa_acumulada_por_nodo):
-    for nodo, masa in masa_acumulada_por_nodo.items():
-        print(f"Nodo {nodo} - Masa asignada: {masa}")
-        ops.mass(nodo, masa, masa, masa)
-
-# --- Acumulación de Masa de Barras en Nodos ---
+# --- Asignación de Masas ---
 def acumular_masa_barras_en_nodos(elements, A, gamma):
-    """
-    Calcula la masa de cada barra y la distribuye entre los nodos de sus extremos.
-    Retorna un diccionario con la masa acumulada por cada nodo.
-    """
     masa_acumulada_por_nodo = {}
 
     for element in elements:
-        nodo_i, nodo_j = element
-        
-        # Obtener las coordenadas de los nodos
+        element_id, nodo_i, nodo_j = element
         coord_i = np.array(ops.nodeCoord(nodo_i))
         coord_j = np.array(ops.nodeCoord(nodo_j))
-        
-        # Calcular la longitud de la barra
         longitud = np.linalg.norm(coord_j - coord_i)
-        
-        # Calcular la masa de la barra
         masa_barra = longitud * A * gamma
-        
-        # Distribuir la mitad de la masa a cada nodo
         masa_acumulada_por_nodo[nodo_i] = masa_acumulada_por_nodo.get(nodo_i, 0) + masa_barra / 2
         masa_acumulada_por_nodo[nodo_j] = masa_acumulada_por_nodo.get(nodo_j, 0) + masa_barra / 2
 
     return masa_acumulada_por_nodo
 
+def acumular_masa_paneles(conexiones_paneles, masa_total, masa_acumulada_por_nodo):
+    num_paneles = len(conexiones_paneles)
+    masa_por_panel = masa_total / num_paneles
+
+    nodos_paneles = []
+    for nodos_conectados in conexiones_paneles:
+        for nodo in nodos_conectados:
+            masa_acumulada_por_nodo[nodo] = masa_acumulada_por_nodo.get(nodo, 0) + masa_por_panel / len(nodos_conectados)
+            nodos_paneles.append(nodo)
+
+    return masa_acumulada_por_nodo, nodos_paneles
+
+def asignar_masas_a_nodos(masa_acumulada_por_nodo, nodos_paneles):
+    for nodo, masa in masa_acumulada_por_nodo.items():
+        if nodo not in nodos_paneles:
+            print(f"Nodo {nodo} - Masa asignada (Barras): {masa}")
+        else:
+            print(f"Nodo {nodo} - Masa asignada (Barras + Panel): {masa}")
+        ops.mass(nodo, masa, masa, masa)
 
 # --- Visualización de la Estructura ---
 def visualizar_estructura_rigida(nodos_paneles, plotter, color='green'):
@@ -188,9 +152,8 @@ def visualizar_estructura_rigida(nodos_paneles, plotter, color='green'):
         surface.faces = [4, 0, 1, 2, 3]
         plotter.add_mesh(surface, color=color, show_edges=True, opacity=0.7)
 
-# --- Graficar Desplazamientos Inerciales ---
-def graficar_desplazamientos_inercia(elements, conexiones_paneles):
-    escala=10
+# --- Graficar Desplazamientos ---
+def graficar_desplazamientos_inercia(elements, conexiones_paneles, escala):
     plotters = [pv.Plotter() for _ in range(3)]
     nodes = np.array([ops.nodeCoord(tag) for tag in ops.getNodeTags()])
     displacements = [
@@ -205,10 +168,9 @@ def graficar_desplazamientos_inercia(elements, conexiones_paneles):
     labels = ['X', 'Y', 'Z']
     for i, plotter in enumerate(plotters):
         truss = pv.PolyData(nodes)
-        truss.lines = np.hstack([[2, e[0] - 1, e[1] - 1] for e in elements])
+        truss.lines = np.hstack([[2, e[1] - 1, e[2] - 1] for e in elements])
 
         truss_displaced = truss.copy()
-        # Aplicar el factor de escala a los desplazamientos
         truss_displaced.points[:, i] += displacements[i] * escala
         plotter.add_mesh(truss, color='blue', label="Estructura Original")
         plotter.add_mesh(truss_displaced, color=colors[i], label=f"Estructura Desplazada en {labels[i]}")
@@ -217,55 +179,15 @@ def graficar_desplazamientos_inercia(elements, conexiones_paneles):
         plotter.show_axes()
         plotter.show()
 
-
-def calcular_masa_total_barras(elements, A, gamma):
-    masa_total = 0
-    for element in elements:
-        nodo_i, nodo_j = element
-        coord_i, coord_j = np.array(ops.nodeCoord(nodo_i)), np.array(ops.nodeCoord(nodo_j))
-        longitud = np.linalg.norm(coord_j - coord_i)
-        masa_barra = longitud * A * gamma
-        masa_total += masa_barra
-    return masa_total
-
-def encontrar_barra(nodo_i, nodo_j, elements):
-    """
-    Encuentra el número de la barra que conecta dos nodos específicos.
-    Parámetros:
-        - nodo_i: ID del primer nodo.
-        - nodo_j: ID del segundo nodo.
-        - elements: Lista de elementos, donde cada elemento es una tupla (element_id, nodo_i, nodo_j).
-    Retorna:
-        - El número de la barra que conecta los nodos, o None si no existe.
-    """
-    for index, (ni, nj) in enumerate(elements):
-        # Comprobar si el elemento conecta los nodos, independientemente del orden
-        if (ni == nodo_i and nj == nodo_j) or (ni == nodo_j and nj == nodo_i):
-            return index + 1  # Devolver el número de barra (basado en índice + 1)
-    return None  # Devolver None si no se encuentra la barra
-
-# --- Deformación Térmica ---
-def variacion_termica(nodos_paneles, elements):
-    termalStrain = alpha * deltaT
-    ops.uniaxialMaterial('InitStrainMaterial', 2, 1, termalStrain)
-
-    for listas in nodos_paneles:
-        for i in range(len(listas)):
-            nodo_i, nodo_j = listas[i], listas[(i + 1) % len(listas)]
-            tag_barra = encontrar_barra(nodo_i, nodo_j, elements)
-            if tag_barra is not None:
-                ops.remove('element', tag_barra)
-                ops.element('Truss', tag_barra, nodo_i, nodo_j, A, 2, '-rho', gamma)
-                
-                #print(f"Material `InitStrainMaterial` con deformación térmica aplicada a la barra {tag_barra}")
-
-def graficar_desplazamientos_termicos(elements, nodos_paneles, escala=1.0):
+def graficar_desplazamientos_termicos(elements, nodos_paneles, escala):
     plotter = pv.Plotter()
     nodes = np.array([ops.nodeCoord(tag) for tag in ops.getNodeTags()])
     deformaciones = [np.array([ops.nodeDisp(tag)[i] for tag in ops.getNodeTags()]) for i in range(3)]
 
+    print("Desplazamientos Termicos:", deformaciones[0])
+
     truss = pv.PolyData(nodes)
-    truss.lines = np.hstack([[2, e[0] - 1, e[1] - 1] for e in elements])
+    truss.lines = np.hstack([[2, e[1] - 1, e[2] - 1] for e in elements])
 
     truss_desplazado = truss.copy()
     for i in range(3):
@@ -274,9 +196,46 @@ def graficar_desplazamientos_termicos(elements, nodos_paneles, escala=1.0):
     plotter.add_mesh(truss, color='blue', label="Estructura Original")
     plotter.add_mesh(truss_desplazado, color='orange', label="Estructura Desplazada Térmicamente")
     visualizar_estructura_rigida(nodos_paneles, plotter, color='gold')
-    plotter.add_text("Desplazamiento Térmico", position='upper_left', font_size=10)
+    plotter.add_text(f"Desplazamiento Térmico (Escala: {escala})", position='upper_left', font_size=10)
     plotter.show_axes()
     plotter.show()
+
+# --- Funciones Auxiliares ---
+def encontrar_barra(nodo_i, nodo_j, elements):
+    for element in elements:
+        element_id, ni, nj = element
+        if (ni == nodo_i and nj == nodo_j) or (ni == nodo_j and nj == nodo_i):
+            return element_id
+    return None
+
+# --- Aplicación de Deformación Térmica ---
+def variacion_termica(conexiones_paneles, elements):
+    ops.timeSeries('Linear', 2)
+    ops.pattern('Plain', 2, 2)
+    thermal_strain = alpha * deltaT
+
+    for nodos_panel in conexiones_paneles:
+        num_nodos = len(nodos_panel)
+        for i in range(num_nodos):
+            nodo_i = nodos_panel[i]
+            nodo_j = nodos_panel[(i + 1) % num_nodos]
+            element_id = encontrar_barra(nodo_i, nodo_j, elements)
+            if element_id is not None:
+                E_element = E
+                A_element = A
+                force_thermal = E_element * A_element * thermal_strain
+
+                coord_i = np.array(ops.nodeCoord(nodo_i))
+                coord_j = np.array(ops.nodeCoord(nodo_j))
+                vector_unitario = (coord_j - coord_i) / np.linalg.norm(coord_j - coord_i)
+
+                fuerza_nodo_i = -force_thermal * vector_unitario
+                fuerza_nodo_j = force_thermal * vector_unitario
+
+                ops.load(nodo_i, *fuerza_nodo_i)
+                ops.load(nodo_j, *fuerza_nodo_j)
+            else:
+                print(f"No se encontró el elemento entre los nodos {nodo_i} y {nodo_j}")
 
 # --- Ejecución del Análisis ---
 def main():
@@ -291,44 +250,24 @@ def main():
     element_id = conectar_nodos_cuadrado(ops.getNodeTags(), element_id, A, 1, gamma, elements)
     element_id = generar_capas(nodes, num_capas, element_id=element_id, A=A, material_id=1, gamma=gamma, elements=elements)
 
-    
+    # Definir material para los paneles
     E_Panel_Solar, material_id_rigido, A_rigido = 1e-1000, 999, 0.01
     ops.uniaxialMaterial('Elastic', material_id_rigido, E_Panel_Solar)
 
+    # Definir conexiones de paneles
     conexiones_paneles = []
     for i in range(num_capas):
         conexiones_paneles.append([2 + 4 * i, 3 + 4 * i, 7 + 4 * i, 6 + 4 * i])
     for nodos_conectados in conexiones_paneles:
         element_id = agregar_estructura_flexible(nodos_conectados, A_rigido, gamma_rigido, element_id, elements, material_id_rigido)
 
-    # Acumular masas de las barras en cada nodo
+    # Acumular y asignar masas
     masa_acumulada_por_nodo = acumular_masa_barras_en_nodos(elements, A, gamma)
-    
-    # Agregar la masa de los paneles a los nodos específicos conectados a los paneles
-    masa_acumulada_por_nodo = acumular_masa_paneles(conexiones_paneles, masa_total, masa_acumulada_por_nodo)
-    
-    # Asignar las masas calculadas a los nodos
-    asignar_masas_a_nodos(masa_acumulada_por_nodo)
+    masa_acumulada_por_nodo, nodos_paneles = acumular_masa_paneles(conexiones_paneles, masa_total, masa_acumulada_por_nodo)
+    asignar_masas_a_nodos(masa_acumulada_por_nodo, nodos_paneles)
 
-    # Aplicar las cargas inerciales
-    #En vez de correr una funcion inercia, aplico una aceleracion a cada nodo segjun su masa, lo que resulta en una fuerza
+    # Aplicar cargas inerciales y realizar análisis estático
     aplicar_cargas_inerciales(masa_acumulada_por_nodo)
-
-    #ops.printModel('node')
-
-    #inercia()
-
-    '''
-    ops.system("BandGen")
-    ops.numberer('RCM')
-    ops.constraints("Plain")
-    ops.integrator("Newmark", 0.5, 0.25)
-    ops.analysis("Transient")
-    ops.analyze(100, 0.01)
-  
-    '''
-
-    # Configuración del análisis estático
     ops.system('BandSPD')
     ops.numberer('RCM')
     ops.constraints('Plain')
@@ -336,20 +275,20 @@ def main():
     ops.algorithm('Linear')
     ops.analysis('Static')
     ops.analyze(1)
-    
 
-    
+    graficar_desplazamientos_inercia(elements, conexiones_paneles, escala=50)
 
-    graficar_desplazamientos_inercia(elements, conexiones_paneles)
-
+    # Aplicar deformación térmica y realizar análisis
     variacion_termica(conexiones_paneles, elements)
-    #graficar_desplazamientos_termicos(elements, conexiones_paneles)
+    ops.system('BandSPD')
+    ops.numberer('RCM')
+    ops.constraints('Plain')
+    ops.integrator('LoadControl', 1.0)
+    ops.algorithm('Linear')
+    ops.analysis('Static')
+    ops.analyze(1)
 
-    #ops.printModel()
-
-    
-
-
+    graficar_desplazamientos_termicos(elements, conexiones_paneles, escala=10)
 
 if __name__ == "__main__":
     main()
