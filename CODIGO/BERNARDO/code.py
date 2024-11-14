0b4ae0d1a4bef677def5b1815b30cd5487abb699
berckanala <bcaprile@miuandes.cl>
2024-11-14 13:51:07 -0300

    avance

--- CODIGO/BERNARDO/code.py
+++ CODIGO/BERNARDO/code.py
@@ -3,7 +3,7 @@ import pyvista as pv
 import openseespy.opensees as ops
 #PARÁMETROS DE ENTRADA
 limite1= 20 #Que tan largo desde la caja es la estructura
-limite2= 48 #Que tan ancha es la estructura
+limite2= 36 #Que tan ancha es la estructura
 
 gamma_fibra_carbono = 1.91 * 1000
 E_fibra_carbono = 338e9
@@ -16,6 +16,130 @@ ops.model('basic', '-ndm', 3, '-ndf', 3)
 
 # Definición de materiales
 ops.uniaxialMaterial('Elastic', 1, E_fibra_carbono)
+
+def plot_paneles_solares(plotter, nodos_paneles_solares1, nodos_paneles_solares2):
+
+    # Procesar los paneles solares en ambas listas
+    for i in range(2):  # Itera sobre las partes izquierda (0) y derecha (1) de cada lista
+        lista1 = nodos_paneles_solares1[i]
+        lista2 = nodos_paneles_solares2[i]
+
+        for nodo in range(len(lista1) - 1):  # Itera hasta el penúltimo nodo para evitar errores de índice
+            # Crear el primer triángulo intercalando los primeros nodos de lista1 y el primero de lista2
+            if nodo < len(lista2):  # Asegura que lista2 tenga suficientes nodos
+                nodos_panel1 = [lista1[nodo], lista1[nodo + 1], lista2[nodo]]
+                
+                surface1 = pv.PolyData(nodos_panel1)
+                surface1.faces = [3, 0, 1, 2]  # Define la cara triangular
+                plotter.add_mesh(surface1, color="gold", show_edges=True, opacity=0.7)
+
+            # Crear el segundo triángulo intercalando los primeros nodos de lista2 y el siguiente de lista1
+            if nodo + 1 < len(lista2):  # Asegura que no exceda el índice de lista2
+                nodos_panel2 = [lista2[nodo], lista2[nodo + 1], lista1[nodo + 1]]
+     
+                surface2 = pv.PolyData(nodos_panel2)
+                surface2.faces = [3, 0, 1, 2]  # Define la cara triangular
+                plotter.add_mesh(surface2, color="gold", show_edges=True, opacity=0.7)
+    
+
+
+
+
+def masa_nodos(nodos_totales, members, gamma_fibra_carbono):
+    masas_nodos = np.zeros(len(nodos_totales))  # Inicializamos una lista con ceros para almacenar la masa de cada nodo
+    A = np.pi * (D1**2 - D2**2) / 4  # Área de la sección transversal (asegúrate de que esté definida en tu contexto)
+    masa_estructura = 0  # Inicializamos la masa total de la estructura
+    for member in members:
+        nodo_i, nodo_j = member  # Extraer los nodos de cada miembro
+        # Calcular la longitud del miembro
+        largo = np.linalg.norm(nodos_totales[nodo_i] - nodos_totales[nodo_j])
+        # Masa del miembro dividido entre los dos nodos
+        masa_miembro = gamma_fibra_carbono * A * largo / 2
+        # Sumar mitad de la masa del miembro a cada nodo
+        masas_nodos[nodo_i] += masa_miembro
+        masas_nodos[nodo_j] += masa_miembro
+        # Sumar la masa total de la estructura
+        masa_estructura += masa_miembro * 2
+
+    print("Masa total de la estructura:", masa_estructura)
+    return masas_nodos, masa_estructura
+
+
+
+def calcular_masa_panel(paneles, nodos_totales):
+    densidad_panel = 1.1  # Densidad del panel (kg/m^2)
+    masas_nodos = np.zeros(len(nodos_totales))  # Inicializamos la masa de cada nodo en 0
+    masa_total = 0  # Inicializamos la masa total de los paneles
+
+    for panel in paneles:
+        # Verifica que el panel tenga tres coordenadas
+        if len(panel) == 3:
+            # Obtiene las coordenadas de los tres puntos del panel
+            punto1, punto2, punto3 = np.array(panel[0]), np.array(panel[1]), np.array(panel[2])
+
+            # Calcula el área del triángulo en 3D
+            vector1, vector2 = punto2 - punto1, punto3 - punto1
+            area_panel = 0.5 * np.linalg.norm(np.cross(vector1, vector2))
+
+            # Calcula la masa del panel
+            masa_panel = densidad_panel * area_panel
+            masa_total += masa_panel
+            masa_nodo = masa_panel / 3  # Distribuye la masa equitativamente entre los tres nodos
+
+            # Encuentra los índices de los nodos en nodos_totales y asigna la masa
+            for punto in [punto1, punto2, punto3]:
+                nodo_index = np.where((nodos_totales == punto).all(axis=1))[0]
+                if nodo_index.size > 0:
+                    masas_nodos[nodo_index[0]] += masa_nodo
+
+        else:
+            print(f"Advertencia: El panel {panel} no tiene exactamente tres puntos.")
+
+    print("Masa total de los paneles solares:", masa_total)
+    
+
+    return masas_nodos, masa_total
+
+
+
+
+def area_paneles_solares(nodos_paneles_solares1, nodos_paneles_solares2):
+    paneles=[]
+    # Procesar los paneles solares en ambas listas
+    for i in range(2):  # Itera sobre las partes izquierda (0) y derecha (1) de cada lista
+        lista1 = nodos_paneles_solares1[i]
+        lista2 = nodos_paneles_solares2[i]
+
+        for nodo in range(len(lista1) - 1):  # Itera hasta el penúltimo nodo para evitar errores de índice
+            # Crear el primer triángulo intercalando los primeros nodos de lista1 y el primero de lista2
+            if nodo < len(lista2):  # Asegura que lista2 tenga suficientes nodos
+                nodos_panel1 = [lista1[nodo], lista1[nodo + 1], lista2[nodo]]
+                paneles.append(nodos_panel1)
+
+            # Crear el segundo triángulo intercalando los primeros nodos de lista2 y el siguiente de lista1
+            if nodo + 1 < len(lista2):  # Asegura que no exceda el índice de lista2
+                nodos_panel2 = [lista2[nodo], lista2[nodo + 1], lista1[nodo + 1]]
+                paneles.append(nodos_panel2)
+
+    area_total=0
+    for panel in paneles:
+        punto1 = np.array(panel[0])
+        punto2 = np.array(panel[1])
+        punto3 = np.array(panel[2])
+
+
+        # Calcular el vector AB y AC
+        vector_ab = punto2 - punto1
+        vector_ac = punto3 - punto1
+
+        # Calcular el producto cruzado de AB y AC
+        producto_cruzado = np.cross(vector_ab, vector_ac)
+
+        # Calcular el área como la mitad de la magnitud del producto cruzado
+        area = 0.5 * np.linalg.norm(producto_cruzado)
+        area_total+=area
+    print("Área total de los paneles solares:", area_total)
+    return paneles
 
 #---------------------------------------------------------------------------------------------------------DEFINICIÓN DEL MODELO
 # Definición inicial de nodos y apoyos
@@ -46,10 +170,11 @@ apoyos_der = np.array([
 
 # Inicializar los nodos y miembros
 nodos_totales = np.vstack((nodos_caja, apoyos_der))
+
 members = np.array([[9, 10], [10, 11], [11, 9]])
 
 # Definición de la función trusselator
-def trusselatorder(limite1, limite2, nodos_totales, members):
+def trusselatorder(limite1, limite2, nodos_totales, members, nodos_paneles_solares):
     paso = 4
     m = len(nodos_totales)  # Contador de nodos, comienza donde terminan los nodos actuales
     for i in range(1, limite1 // paso+1):
@@ -59,7 +184,7 @@ def trusselatorder(limite1, limite2, nodos_totales, members):
         a = [
             [L1, L2, L0 + i * paso],
             [-L1, L2, L0 + i * paso],
-            [0, -L2, L0 + i * paso]
+            [0, -L2, L0 + i * paso] #añado el último
         ]
         mbr = np.array([
             [m, m + 1], [m + 1, m + 2], [m + 2, m],  # Cierra el triángulo
@@ -69,6 +194,8 @@ def trusselatorder(limite1, limite2, nodos_totales, members):
         nodos_totales = np.vstack((nodos_totales, a))
         members = np.vstack((members, mbr))
         m += 3  # Incrementa el contador de nodos
+    nodos_paneles_solares[0].append(a[2])
+    nodos_paneles_solares[1].append(a[2])
     a=np.array([[0, 0, L0 + (i+1)*paso]])
     nodos_totales = np.vstack((nodos_totales, a))
     members = np.vstack((members, [[m, m-3], [m, m-2], [m, m-1]]))
@@ -80,8 +207,9 @@ def trusselatorder(limite1, limite2, nodos_totales, members):
         a=[
                 [L1+paso*i, L2, L0 ],
                 [L1+paso*i, 0, L0+ paso],
-                [L1 +paso*i, -L2, L0 ]
+                [L1 +paso*i, -L2, L0 ] #A este se le va a anclar los paneles solares
             ]
+        nodos_paneles_solares[0].append(a[2])
         nodos_totales = np.vstack((nodos_totales, a))
         if i==1:
             mbr= np.array([[m, m+1], [m+1, m+2], [m+2, m], #Se cierra el triángulo
@@ -100,9 +228,10 @@ def trusselatorder(limite1, limite2, nodos_totales, members):
         a=[
                 [-L1-paso*i, L2, L0 ],
                 [-L1-paso*i, 0, L0+ paso],
-                [-L1 -paso*i, -L2, L0 ]
+                [-L1 -paso*i, -L2, L0 ] #A este se le va a anclar los paneles solares
             ]
         nodos_totales = np.vstack((nodos_totales, a))
+        nodos_paneles_solares[1].append(a[2])
         if i==1:
             mbr= np.array([[m, m+1], [m+1, m+2], [m+2, m], #Se cierra el triángulo
                             [m, m_1], [m, m_1-1], [m, m_1-2],
@@ -116,9 +245,9 @@ def trusselatorder(limite1, limite2, nodos_totales, members):
         m+=3
         members = np.vstack((members, mbr))
    
-    return nodos_totales, members
+    return nodos_totales, members, nodos_paneles_solares
 
-def trusselatorizq(limite1, limite2, nodos_totales, members):
+def trusselatorizq(limite1, limite2, nodos_totales, members, nodos_paneles_solares):
     paso = 4
     m = len(nodos_totales) 
     for i in range(1, limite1 // paso+1):
@@ -138,6 +267,8 @@ def trusselatorizq(limite1, limite2, nodos_totales, members):
         nodos_totales = np.vstack((nodos_totales, a))
         members = np.vstack((members, mbr))
         m += 3  # Incrementa el contador de nodos
+    nodos_paneles_solares[0].append(a[2])
+    nodos_paneles_solares[1].append(a[2])
     a=np.array([[0, 0, L0 - (i+1)*paso]])
     nodos_totales = np.vstack((nodos_totales, a))
     members = np.vstack((members, [[m, m-3], [m, m-2], [m, m-1]]))
@@ -149,8 +280,9 @@ def trusselatorizq(limite1, limite2, nodos_totales, members):
         a=[
                 [L1+paso*i, L2, L0 ],
                 [L1+paso*i, 0, L0- paso],
-                [L1 +paso*i, -L2, L0 ]
+                [L1 +paso*i, -L2, L0 ] #A este se le va a anclar los paneles solares
             ]
+        nodos_paneles_solares[0].append(a[2])
         nodos_totales = np.vstack((nodos_totales, a))
         if i==1:
             mbr= np.array([[m, m+1], [m+1, m+2], [m+2, m], #Se cierra el triángulo
@@ -169,8 +301,9 @@ def trusselatorizq(limite1, limite2, nodos_totales, members):
         a=[
                 [-L1-paso*i, L2, L0 ],
                 [-L1-paso*i, 0, L0- paso],
-                [-L1 -paso*i, -L2, L0 ]
+                [-L1 -paso*i, -L2, L0 ] #A este se le va a anclar los paneles solares
             ]
+        nodos_paneles_solares[1].append(a[2])
         nodos_totales = np.vstack((nodos_totales, a))
         if i==1:
             mbr= np.array([[m, m+1], [m+1, m+2], [m+2, m], #Se cierra el triángulo
@@ -185,11 +318,11 @@ def trusselatorizq(limite1, limite2, nodos_totales, members):
         m+=3
         members = np.vstack((members, mbr))
    
-    return nodos_totales, members
+    return nodos_totales, members, nodos_paneles_solares
 
 # Llamar a la función trusselator
-
-nodos_totales, members = trusselatorder(limite1, limite2, nodos_totales, members)
+nodos_paneles_solares=[[],[]]
+nodos_totales, members, nodos_paneles_solares1 = trusselatorder(limite1, limite2, nodos_totales, members, nodos_paneles_solares)
 
 m= len(nodos_totales)+1
 
@@ -203,8 +336,9 @@ mbr= [[m, m+1], [m+1, m+2], [m+2, m]]
 
 nodos_totales=np.vstack((nodos_totales, apoyos_der))
 members=np.vstack((members, mbr))
+nodos_paneles_solares=[[],[]]
+nodos_totales, members, nodos_paneles_solares2 = trusselatorizq(limite1, limite2, nodos_totales, members, nodos_paneles_solares)
 
-nodos_totales, members = trusselatorizq(limite1, limite2, nodos_totales, members)
 #---------------------------------------------------------------------------------------------------------DEFINICIÓN DEL MODELO
 #---------------------------------------------------------------------------------------------------------EJECUCIÓN DEL MODELO
 for i, nodo in enumerate(nodos_totales):
@@ -214,6 +348,24 @@ for i, member in enumerate(members):
     nodo_i = int(member[0] + 1)
     nodo_j = int(member[1] + 1)
     ops.element('Truss', i + 1, nodo_i, nodo_j, A, 1, '-rho', gamma_fibra_carbono)
+
+#---------------------------------------------------------------------------------------------------------Masa de los nodos
+masas_nodos, masa_estructura = masa_nodos(nodos_totales, members, gamma_fibra_carbono)
+for i, masa in enumerate(masas_nodos, start=1):  # Enumeración comienza en 1 porque los nodos en OpenSees también lo hacen
+    ops.mass(i, masa, masa, masa)
+
+paneles= area_paneles_solares(nodos_paneles_solares1, nodos_paneles_solares2)
+
+masas_nodos, masa_total = calcular_masa_panel(paneles, nodos_totales)
+
+# Asignar las masas adicionales en OpenSees
+for i, masa in enumerate(masas_nodos, start=1):
+    ops.mass(i, masa, masa, masa)
+
+RME= masa_estructura/masa_total
+
+print("RME de la estructura:", RME)
+
 #---------------------------------------------------------------------------------------------------------EJECUCIÓN DEL MODELO
 plotter = pv.Plotter()
 
@@ -234,7 +386,9 @@ for member in members:
     line = pv.Line(start_node, end_node)
     plotter.add_mesh(line, color="red", line_width=2)
 
+plot_paneles_solares(plotter, nodos_paneles_solares1, nodos_paneles_solares2)
+
 # Visualización final
 plotter.add_axes()
-plotter.show()
+# plotter.show()
 
